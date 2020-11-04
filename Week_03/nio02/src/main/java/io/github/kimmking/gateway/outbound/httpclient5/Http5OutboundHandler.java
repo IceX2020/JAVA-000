e41fb0909505a15a024f9cd95ad4fb50ce85b7f4
IceX2020 <IceX2020@163.com>
2020-11-04 22:18:51 +0800

    作业step3，将请求的报文头设置到新的请求中

--- Week_03/nio02/src/main/java/io/github/kimmking/gateway/outbound/httpclient5/Http5OutboundHandler.java
+++ Week_03/nio02/src/main/java/io/github/kimmking/gateway/outbound/httpclient5/Http5OutboundHandler.java
@@ -1,26 +1,22 @@
 package io.github.kimmking.gateway.outbound.httpclient5;
 
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpUtil;
-import org.apache.hc.client5.http.ClientProtocolException;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.http.HttpStatus;
-import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.HttpClientResponseHandler;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.http.protocol.HTTP;
+
+import java.io.IOException;
 
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
@@ -34,54 +30,44 @@ public class Http5OutboundHandler {
     }
 
     public void handle(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx) throws IOException {
-        // Create a custom response handler
-        FullHttpResponse fhresponse = null;
         String responseBody = "";
-
+//        System.out.println(fullRequest);
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
             HttpGet httpget = new HttpGet("http://127.0.0.1:8088/api/hello");
-
-            System.out.println("Executing request " + httpget.getMethod() + " " + httpget.getUri());
-
+            for(String key: (fullRequest.headers().names())){
+                if(key.equals("content-length")){
+                    continue;
+                }
+                String value = fullRequest.headers().get(key);
+                httpget.setHeader(key,value);
+//                System.out.println(key+":"+value);
+            }
+            httpget.setHeader(HTTP.CONN_DIRECTIVE, HTTP.CONN_KEEP_ALIVE);
             HttpClientResponseHandler<String> responseHandler = new HttpClientResponseHandler<String>() {
-
                 @Override
                 public String handleResponse(ClassicHttpResponse response) throws IOException {
                     int status = response.getCode();
                     if (status >= HttpStatus.SC_SUCCESS && status < HttpStatus.SC_REDIRECTION) {
                         final HttpEntity entity = response.getEntity();
-                        try {
-                            return entity != null ? EntityUtils.toString(entity,"UTF-8") : null;
-                        } catch (final ParseException ex) {
-                            throw new ClientProtocolException(ex);
+                        byte[] body = EntityUtils.toByteArray(entity);
+                        DefaultFullHttpResponse fhresponse = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(body));
+                        fhresponse.headers().set("Content-Type", "application/json");
+                        fhresponse.headers().setInt("Content-Length", Integer.parseInt(response.getFirstHeader("Content-Length").getValue()));
+                        if (fullRequest != null) {
+                            if (!HttpUtil.isKeepAlive(fullRequest)) {
+                                ctx.write(fhresponse).addListener(ChannelFutureListener.CLOSE);
+                            } else {
+                                ctx.write(fhresponse);
+                            }
                         }
+                        ctx.flush();
                     } else {
-                        throw new ClientProtocolException("Unexpected response status: " + status);
+                        exceptionCaught(ctx,null);
                     }
+                    return null;
                 }
-
             };
-            responseBody = httpclient.execute(httpget, responseHandler);
-            System.out.println("----------------------------------------");
-            System.out.println(responseBody);
-
-            fhresponse = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(responseBody.getBytes()));
-            fhresponse.headers().set("Content-Type", "application/json");
-            fhresponse.headers().setInt("Content-Length", responseBody.getBytes().length);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            exceptionCaught(ctx,e);
-        } finally {
-            if (fullRequest != null) {
-                if (!HttpUtil.isKeepAlive(fullRequest)) {
-                    ctx.write(fhresponse).addListener(ChannelFutureListener.CLOSE);
-                } else {
-                    //response.headers().set(CONNECTION, KEEP_ALIVE);
-                    ctx.write(fhresponse);
-                }
-            }
-            ctx.flush();
-            //ctx.close();
+            responseBody = httpclient.execute(httpget,responseHandler);
         }
     }
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
